1305b118fcfeaf4d673d8531b87a1724eb64505f
Karankaleeswaran <161699022+Karankaleeswaran@users.noreply.github.com>
2024-09-02 09:05:19 +0530

    Add files via upload

---
+++ automation.js
@@ -0,0 +1,6 @@
+
+       
+document.getElementById('automationcourse').addEventListener('click', function() {
+    window.location.href = "file:///C:/Users/Dhanushiya/Desktop/Karan/project/Project/Sample%20Project/final/Automation.html"
+});
+        
