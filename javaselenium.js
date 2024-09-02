c3717ad65122010e6694dc1fbb90354369d1ce77
Karankaleeswaran <161699022+Karankaleeswaran@users.noreply.github.com>
2024-09-02 09:06:03 +0530

    Add files via upload

---
+++ javaselenium.js
@@ -0,0 +1,6 @@
+
+       
+document.getElementById('javaseleniumcourse').addEventListener('click', function() {
+    window.location.href = "file:///C:/Users/Dhanushiya/Desktop/Karan/project/Project/Sample%20Project/final/Javaselenium.html"
+});
+        
